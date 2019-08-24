890ee4ac061863a3c8eb0fb53dd1173c0271a3a7
Erik Corona <ecorona@live.com>
2019-08-24 13:14:04 -0500

    docu adicional encontrada por ahi

--- tracker.js
+++ tracker.js
@@ -105,3 +105,67 @@ Tracker.prototype.moveAlarm = function(){
 };
 
 module.exports = Tracker;
+
+/*
+Get current position ( 1 position only )
+** , imei : 999999999999999 , B ;
+
+Set multiple positions
+** , imei : 999999999999999 , C , # # x ;
+    where # # is a number from 01 to 99
+    and x is s for seconds , m for minutes , h for hours
+        Example : set location at every 2 minutes
+        ** , imei : 999999999999999 , C , 02m ;
+
+Stop sending positions
+** , imei : 999999999999999 , A ;
+
+Stop sending alarm messages (door alarm , acc alarm , power alarm , SOS alarm)
+** , imei : 999999999999999 , E;
+
+Set positioning by distance ( only if vehicle tracker sends position has traveled XXXX meters )
+** , imei : 999999999999999 , F , XXXXm ;
+    where XXXX is the distance in meters (you can September less than 200m but the minimum will always be about 200m )
+
+Activate the alarm movement ( sends SMS if unit moves 200m )
+** , imei : 999999999999999 , G ;
+
+Activate the speed alarm ( sends SMS if speed goes above XXX km / h )
+** , imei : 999999999999999 , H , XXX ;
+    where XXX is the speed in km / h
+
+Set the timezone to GMT +0 (this only works enquiry.c tracker on gps -trace with timezone set to +0
+** , imei : 999999999999999 , I +0;
+    I is capitol i
+
+Stop / block the engine
+** , imei : 999999999999999 , J ;
+
+Resume / unblock the engine
+** , imei : 999999999999999 , K ;
+
+Arm alarm (door , acc , shock sensor)
+** , imei : 999999999999999 , L ;
+
+Disarm alarm (door , acc , shock sensor)
+** , imei : 999999999999999 , M ;
+
+Turn off GPRS ( returns to SMS mode . This can only be undone by sending an SMS )
+** , imei : 999999999999999 , N ;
+
+Create a Geofence alarm between points A, B and C , D
+** , IMEI : 999999999999999 O, A, B , C, D;
+    Example:
+    ** , imei : 012497000324230 , O, -30.034173 , -051.167557 , -30.044679 , -051.146198 ;
+
+Deactivate geofence
+** , imei : 999999999999999 , P ;
+
+Request upload of SD card saved points ( only on tracker with sd card )
+** , imei : 999999999999999 , Q , XXXXXXXX ;
+    where XXXXXXXX is the date in yyyymmdd format
+
+Cancel upload of SD card saved points
+** , imei : 999999999999999 , S ;
+
+*/
